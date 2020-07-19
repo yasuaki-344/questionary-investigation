96c96f455ac8315af23a1a17fdec29a03150ac4d
yasuaki-miyoshi <ikusay.443@gmail.com>
2020-07-19 17:58:30 +0900

    Add Support for ASP.NET Core Identity
    This commit is to add Support for ASP.NET Core Identity.

---
+++ src/Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using QuestionaryInvestigation.Infrastructure.Data;
+using System;
+
+[assembly: HostingStartup(typeof(Web.Areas.Identity.IdentityHostingStartup))]
+namespace Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) =>
+            {
+            });
+        }
+    }
+}
